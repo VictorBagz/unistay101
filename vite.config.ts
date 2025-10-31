6477bf60bfa7425ff2100eb1622ce1f5f1872ca6
VictorBagz <victorbaguma@example.com>
2025-10-31 18:01:40 +0300

    all changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     const isProd = mode === 'production';
 
     return {
-      base: '/unistay101/', 
+      base: '/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
